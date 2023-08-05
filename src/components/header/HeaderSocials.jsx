c252f5b3280eb473db001f611018bd61460532ee
Arun Acharya <113877370+arunacharya1603@users.noreply.github.com>
2023-08-05 11:22:45 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-    <a href="https://linkdin.com" target='_blank'><BsLinkedin /></a>
-    <a href="https://github.com" target='_blank'><FaGithub /></a>
+    <a href="https://www.linkedin.com/in/arunacharya1603/" target='_blank'><BsLinkedin /></a>
+    <a href="https://github.com/arunacharya1603" target='_blank'><FaGithub /></a>
     <a href="" target='_blank'></a>
       
     </div>
