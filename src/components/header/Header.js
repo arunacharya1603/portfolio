d763566cd9e006e6be215516b779df49cd5044ac
Arun Acharya <113877370+arunacharya1603@users.noreply.github.com>
2023-09-28 12:38:42 +0530

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.jpg'
+import ME from '../../assets/me-about.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
       <header>
-        <div className="container header__container"><h5>Hello I'm </h5>
+        <div class="container header__container"><h5>Hello I'm </h5>
         <h1>Arun Acharya</h1>
         <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
